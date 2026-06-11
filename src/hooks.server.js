dad095c5a37d6ddb2f4f1949e69df32b10c6f8d7
Rei007 <alijajrei@gmail.com>
2026-06-11 14:50:38 +0200

    hooks.server.js

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -4,4 +4,5 @@ export async function handle({ event, resolve }) {
 	const sessionId = event.cookies.get('session');
 	event.locals.user = sessionId ? await validateSession(sessionId) : null;
 	return resolve(event);
+	
 }
